a9b18277ec3edcd153d4d655d6e988c81823efab
JustNekoChris <chbarajas20@gmai.com>
2024-07-15 11:59:22 -0700

    Files.Java is now only Play Framework Code and FilesService.java is now Non Play Framework Code

--- app/controllers/FilesService.java
+++ app/controllers/FilesService.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.lang.System.Logger;
 import java.net.URL;
 import java.nio.file.Path;
+import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.script.ScriptException;
@@ -19,8 +21,8 @@ import models.CodeCheck;
 
 public class FilesService {
     @Inject private CodeCheck codeCheck;    
-    private static Logger logger = System.getLogger("com.horstmann.codecheck");
-    @Inject private Config config;          
+    @Inject private Config config;     
+    private static Logger logger = System.getLogger("com.horstmann.codecheck");     
     
     String start2 = "<!DOCTYPE html>\n<html><head>\n"
             + "<title>CodeCheck</title>"
@@ -43,9 +45,8 @@ public class FilesService {
             + "</body>\n" 
             + "</html>";
 
-    public String resultMaker(String url, String repo, String problemName, String ccid) 
+    public String filesHTML2_2(String url, String repo, String problemName, String ccid) 
             throws IOException, NoSuchMethodException, ScriptException{
-
         Map<Path, byte[]> problemFiles = getProblemFiles(problemName, repo, ccid);
         if (problemFiles.isEmpty()) // if problemFiles is empty, return "0" to indicate error
             return "0";
@@ -72,22 +73,24 @@ public class FilesService {
     }
     
     private static String tracerStart = "<!DOCTYPE html>\n"
-            + "<html>\n"
-            + "<head>\n"
-            + "  <meta charset=\"utf-8\">\n"
-            + "  <link href='https://horstmann.com/codecheck/css/codecheck_tracer.css' rel='stylesheet' type='text/css'/>  "
-            + "  <title>CodeCheck Tracer</title>\n"
-            + "  <script src='/assets/util.js'></script>\n"
-            + "  <script src='/assets/codecheck2.js'></script>\n"
-            + "</head>\n"
-            + "<body>\n";
+                                      + "<html>\n"
+                                      + "<head>\n"
+                                      + "  <meta charset=\"utf-8\">\n"
+                                      + "  <link href='https://horstmann.com/codecheck/css/codecheck_tracer.css' rel='stylesheet' type='text/css'/>  "
+                                      + "  <title>CodeCheck Tracer</title>\n"
+                                      + "  <script src='/assets/util.js'></script>\n"
+                                      + "  <script src='/assets/codecheck2.js'></script>\n"
+                                      + "</head>\n"
+                                      + "<body>\n";
+
     private static String tracerScriptStart = "    <div class='codecheck_tracer'>\n"
-            + "      <script type='module'>//<![CDATA[\n";
+                                            + "      <script type='module'>//<![CDATA[\n";
+                                            
     private static String tracerEnd = "// ]]>\n"
-            + "      </script>\n"
-            + "  </div>\n"
-            + "</body>\n"
-            + "</html>";
+                                    + "      </script>\n"
+                                    + "  </div>\n"
+                                    + "</body>\n"
+                                    + "</html>";
     
 	public String tracer2(String ccid, Map<Path, byte[]> problemFiles) throws IOException {
         if (problemFiles.isEmpty())
@@ -131,5 +134,138 @@ public class FilesService {
         } catch (IOException e) {
             e.printStackTrace();
         } }).start();
+    }
+
+    // TODO: Legacy, also codecheck.js
+    private static String start = "<!DOCTYPE html>\n<html><head>\n"
+                                + "<meta http-equiv='content-type' content='text/html; charset=UTF-8' />\n"
+                                + "<script src='/assets/download.js'></script>\n" 
+                                + "<script src='/assets/ace/ace.js'></script>\n"
+                                + "<script src='/assets/codecheck.js'></script>\n"
+                                + "<link type='text/css' rel='stylesheet' href='/assets/codecheck.css'/>\n" 
+                                + "</head><body>\n";
+    private static String before = "<form method=\"post\" action=\"{0}\">\n";
+    private static String fileAreaBefore = "<div id=\"{0}\" name=\"{0}\" rows=\"{1}\" cols=\"80\" class=\"editor {2}\">";
+    private static String fileAreaBeforeNoEdit = "<div id=\"{0}\" name=\"{0}\" rows=\"{1}\" cols=\"{2}\" class=\"editor readonly {3}\">";
+    private static String fileAreaAfter = "</div>\n";
+    private static String fileOuterDiv = "<div id=\"{0}\" class=\"file\">\n";
+    private static String fileOuterDivAfter = "</div>\n";
+    private static String after = "<div><input id=\"submit\" type=\"submit\"/>\n"
+                                + "<input type=\"hidden\" name=\"repo\" value=\"{0}\"/>\n"
+                                + "<input type=\"hidden\" name=\"problem\" value=\"{1}\"/>\n";
+    private static String formEnd = "</form>\n<div id=\"codecheck-submit-response\"></div>\n";
+    private static String bodyEnd = "</body></html>";
+    private static String useStart = "<p>Use the following {0,choice,1#file|2#files}:</p>\n";
+    private static String provideStart = "<p>Complete the following {0,choice,1#file|2#files}:</p>\n";
+
+    public String filesHTML_2(String repo, String problemName, String ccid) 
+    throws IOException, NoSuchMethodException, ScriptException {
+        Map<Path, byte[]> problemFiles = getProblemFiles(problemName, repo, ccid);
+        if (problemFiles.isEmpty()) // if problemFiles is empty, return "0" to indicate error
+            return "0";
+        Problem problem = new Problem(problemFiles);
+        Problem.DisplayData data = problem.getProblemData();
+        StringBuilder result = new StringBuilder();
+        result.append(start);
+
+        if (data.description != null)
+            result.append(data.description);
+        String contextPath = ""; // request.host(); // TODO
+        String url = contextPath + "/check";
+        result.append(MessageFormat.format(before, url));
+        result.append(MessageFormat.format(provideStart, data.requiredFiles.size()));
+
+        for (Map.Entry<String, Problem.EditorState> entry : data.requiredFiles.entrySet()) {
+            String file = entry.getKey();
+            List<String> conts = entry.getValue().editors;
+
+            if (file.equals("Input") && conts.get(0).trim().length() == 0) {
+                // Make a hidden field with blank input
+                result.append("<input type='hidden' name='Input' value=''/>");
+            } else {
+                boolean firstTitle = true;
+                int textAreaNumber = 0;
+                String appended;
+                // int continuingLines = 0;
+                boolean editable = true;
+                for (String cont : conts) {
+                    if (cont == null) { // only the case for the first time to skip editable
+                        editable = false;
+                    } else {
+                        int lines = 0;
+                        textAreaNumber++;
+                        appended = file + "-" + textAreaNumber;
+                        lines = Util.countLines(cont);
+                        if (lines == 0)
+                            lines = 20;
+
+                        if (editable) {
+                            if (firstTitle) {
+                                result.append(MessageFormat.format(fileOuterDiv, file));
+                                result.append("<h3>");
+                                result.append(file);
+                                result.append("</h3>");
+                                firstTitle = false;
+                            }
+                            // result.append(MessageFormat.format(startNumberLines, "editor",
+                            // "firstLineNumber", continuingLines));
+                            result.append(MessageFormat.format(fileAreaBefore, appended, lines, "java"));
+                            // TODO support more than "java" in ace editor format
+                            result.append(Util
+                                    .removeTrailingNewline(Util.htmlEscape(cont)));
+                            result.append(fileAreaAfter);
+                            editable = false;
+                        } else {
+                            if (firstTitle) {
+                                result.append(MessageFormat.format(fileOuterDiv, file));
+                                result.append("<h3>");
+                                result.append(file);
+                                result.append("</h3>");
+                                firstTitle = false;
+                            }
+
+                            String s = cont;
+                            int max = 20;
+                            while (s.indexOf("\n") != -1) {
+                                if ((s.substring(0, s.indexOf("\n"))).length() > max) {
+                                    max = (s.substring(0, s.indexOf("\n"))).length();
+                                }
+                                s = s.substring(s.indexOf("\n") + 1);
+                            }
+                            if (s.length() > max) {
+                                max = s.length();
+                            }
+
+                            result.append(MessageFormat.format(fileAreaBeforeNoEdit, appended, lines, max, "java")); 
+                            // TODO: support more than "java" in ace editor format
+                            result.append(Util
+                                    .removeTrailingNewline(Util.htmlEscape(cont)));
+                            result.append(fileAreaAfter);
+                            editable = true;
+                        }
+                    }
+                }
+                result.append(fileOuterDivAfter);
+            }
+        }
+        result.append(MessageFormat.format(after, repo, problemName));
+        result.append(formEnd);
+
+        int nusefiles = data.useFiles.size();
+        if (nusefiles > 0) {
+            result.append(MessageFormat.format(useStart, nusefiles));
+            for (Map.Entry<String, String> entry : data.useFiles.entrySet()) {
+                result.append("<p>");
+                result.append(entry.getKey());
+                result.append("</p>\n");
+                result.append("<pre>");
+                result.append(Util.htmlEscape(entry.getValue()));
+                result.append("</pre\n>");
+            }
+        }
+
+        // result.append(jsonpAjaxSubmissionScript);
+        result.append(bodyEnd);
+        return result.toString();
     }
 }
